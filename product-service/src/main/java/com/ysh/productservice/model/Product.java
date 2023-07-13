9fb0b461755be875c1aaa044b54cfb7792a9694c
YoonSuHyeon <96-05-03@naver.com>
2023-07-13 22:36:21 +0900

    productservice 초기 프로젝트 설정

---
+++ product-service/src/main/java/com/ysh/productservice/model/Product.java
@@ -0,0 +1,15 @@
+package com.ysh.productservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
